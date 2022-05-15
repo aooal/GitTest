bb10544320e1166728ad6307ea4bab5943cba2ce
aooal <taboi40145@gmail.com>
2022-05-16 00:07:42 +0800

    test2

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace GitTest.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "hello";
+            ViewBag.Message = "test";
             return View();
         }
 
